cff1bf6b3375c1597c84f40e2a588c031bbbb6bc
Ananda Bayu <anandabayu090@gmail.com>
2022-11-09 13:21:56 +0700

    responsive table data

--- src/components/User-List-Table.js
+++ src/components/User-List-Table.js
@@ -10,40 +10,44 @@ export default function UserListTable(props) {
 	};
 
 	return (
-		<table className="table">
-			<thead className="text-center">
-				<tr style={style.text}>
-					<th scope="col">USERNAME</th>
-					<th scope="col">EMAIL</th>
-					<th scope="col">PHONE</th>
-					<th scope="col"></th>
-				</tr>
-			</thead>
-			<tbody>
-				{props.users &&
-					props.users.map((el, index) => {
-						return (
-							<tr
-								key={index}
-								style={style.text}
-								className="text-center"
-							>
-								<td>{el.username}</td>
-								<td>{el.email}</td>
-								<td>{el.phone}</td>
-								<td className="text-center">
-									<button
-										className="btn btn-light"
-										style={style.button}
-										onClick={() => props.handleViewUser(el)}
-									>
-										View Detail
-									</button>
-								</td>
-							</tr>
-						);
-					})}
-			</tbody>
-		</table>
+		<div class="table-responsive">
+			<table className="table">
+				<thead className="text-center">
+					<tr style={style.text}>
+						<th scope="col">USERNAME</th>
+						<th scope="col">EMAIL</th>
+						<th scope="col">PHONE</th>
+						<th scope="col"></th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.users &&
+						props.users.map((el, index) => {
+							return (
+								<tr
+									key={index}
+									style={style.text}
+									className="text-center"
+								>
+									<td>{el.username}</td>
+									<td>{el.email}</td>
+									<td>{el.phone}</td>
+									<td className="text-center">
+										<button
+											className="btn btn-light"
+											style={style.button}
+											onClick={() =>
+												props.handleViewUser(el)
+											}
+										>
+											View Detail
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+				</tbody>
+			</table>
+		</div>
 	);
 }
